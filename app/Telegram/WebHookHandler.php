b535245491f65840c91644f70da9de93fd7e247d
Peter Mwaura <kimmwaus@gmail.com>
2024-06-17 04:51:11 +0300

    installed telegraph with value

--- app/Telegram/WebHookHandler.php
+++ app/Telegram/WebHookHandler.php
@@ -5,19 +5,39 @@ namespace App\Telegram;
 use DefStudio\Telegraph\Models\TelegraphBot;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use PHPUnit\Event\Code\Throwable;
 use Stringable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class WebHookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
-    public function handle(Request $request, TelegraphBot $bot): void
+//    public function handle(Request $request, TelegraphBot $bot): void
+//    {
+//        Log::info("active");
+//    }
+
+//    protected function handleChatMessage(Stringable $text): void
+//    {
+//        // in this example, a received message is sent back to the chat
+//        $this->chat->html("Received: $text")->send();
+//    }
+
+    public function hi(): void
     {
-        Log::info("active");
+        $this->chat->markdown('*Hi* happy to be here!')->send();
     }
 
-    protected function handleChatMessage(Stringable $text): void
+    /**
+     * @throws \Throwable
+     */
+    protected function onFailure(Throwable|\Throwable $throwable): void
     {
-        // in this example, a received message is sent back to the chat
-        $this->chat->html("Received: $text")->send();
-    }
+        if ($throwable instanceof NotFoundHttpException) {
+            throw $throwable;
+        }
 
+        report($throwable);
+
+        $this->reply('sorry man, I failed');
+    }
 }
