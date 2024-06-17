0a28a63e33d8c59612729c5321b6c4c5d6b68274
Peter Mwaura <kimmwaus@gmail.com>
2024-06-17 04:43:00 +0300

    installed telegraph with value

--- app/Telegram/WebHookHandler.php
+++ app/Telegram/WebHookHandler.php
@@ -5,12 +5,19 @@ namespace App\Telegram;
 use DefStudio\Telegraph\Models\TelegraphBot;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Stringable;
 
 class WebHookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
     public function handle(Request $request, TelegraphBot $bot): void
     {
         Log::info("active");
+    }
+
+    protected function handleChatMessage(Stringable $text): void
+    {
+        // in this example, a received message is sent back to the chat
+        $this->chat->html("Received: $text")->send();
     }
 
 }
