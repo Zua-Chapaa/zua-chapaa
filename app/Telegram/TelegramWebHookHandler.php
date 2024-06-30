d13e1d4085f2ea10ecd686ebc89cf76b6248598a
Peter Mwaura <kimmwaus@gmail.com>
2024-06-30 09:11:37 -0700

    linked app

--- app/Telegram/TelegramWebHookHandler.php
+++ app/Telegram/TelegramWebHookHandler.php
@@ -4,6 +4,10 @@ namespace App\Telegram;
 
 class TelegramWebHookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
+    public function start():void
+    {
+        Log::info("here");
+    }
     protected function onFailure(\Throwable $throwable): void
     {
         if ($throwable instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
