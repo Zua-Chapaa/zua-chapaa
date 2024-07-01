b6adb49203e7fcd84aa569eeb82fdb45644b46b6
Peter Mwaura <kimmwaus@gmail.com>
2024-07-01 00:38:54 -0700

    update x
    Signed-off-by: Peter Mwaura <kimmwaus@gmail.com>

--- app/Telegram/CallBacks/HandleChatMessage.php
+++ app/Telegram/CallBacks/HandleChatMessage.php
@@ -38,44 +38,54 @@ trait HandleChatMessage
 
     private function goToHome($text = null): void
     {
-        $this->getChat()->message(empty($this->getChat()->storage()->get('user_context')))->send();
+        if (
+            !empty($this->getChat()->storage()->get('user_context')) &&
+            $this->getChat()->storage()->get('user_context') == 'phone_number_request_mode'
+        ) {
 
-//        if ($this->getChat()->storage()->get('user_context') && $this->getChat()->storage()->get('user_context') == 'phone_number_request_mode') {
-//            $number_is_valid = $this->validateNumber($text);
-//
-//            if (!$number_is_valid) {
-//                $this->getChat()->storage()->set('user_context', "");
-//                $this?->getChat()
-//                    ->message(
-//                        "The number you entered was invalid use either \n
-//                        07********
-//                        +2547********
-//                        2547********
-//                        Formats
-//                        ")
-//                    ->keyboard(Keyboard::make()->row([
-//                        Button::make('Try Again')
-//                            ->action('invalid_phone_number_action')
-//                            ->param('action', 'Try Again'),
-//
-//                        Button::make('Cancel')
-//                            ->action('invalid_phone_number_action')
-//                            ->param('action', 'Cancel'),
-//                    ]))->send();
-//            }
-//            return;
-//        }
+            if (!$this->validateNumber($text)) {
 
-//        $this->getChat()->message('Select a plan to proceed')
-//            ->keyboard(Keyboard::make()->row([
-//                Button::make('Hourly Plan @Ksh 100')
-//                    ->action('select_plan')
-//                    ->param('plan', 'hourly'),
-//
-//                Button::make('Day Plan @Ksh 1500')
-//                    ->action('select_plan')
-//                    ->param('plan', 'daily'),
-//            ]))->send();
+                //reset context
+                $this->getChat()->storage()->set('user_context', "");
+
+                //Advice user on corrections
+                $this?->getChat()
+                    ->message(
+                        "The number you entered was invalid use either \n
+                        07********
+                        +2547********
+                        2547********
+                        Formats
+                        ")
+                    ->keyboard(Keyboard::make()->row([
+                        Button::make('Try Again')
+                            ->action('invalid_phone_number_action')
+                            ->param('action', 'Try Again'),
+
+                        Button::make('Cancel')
+                            ->action('invalid_phone_number_action')
+                            ->param('action', 'Cancel'),
+                    ]))->send();
+            }else{
+//                make a payment request
+                $this->getChat()->message("Please accept the payment request made to continue.");
+            }
+        }else{
+            //application not in subscription mode
+
+            $this->getChat()->message('Select a plan to proceed')
+                ->keyboard(Keyboard::make()->row([
+                    Button::make('Hourly Plan @Ksh 100')
+                        ->action('select_plan')
+                        ->param('plan', 'hourly'),
+
+                    Button::make('Day Plan @Ksh 1500')
+                        ->action('select_plan')
+                        ->param('plan', 'daily'),
+                ]))->send();
+        }
+
+
     }
 
     private function goToLeadersBoard()
