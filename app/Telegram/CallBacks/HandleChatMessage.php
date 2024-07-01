ee8ee80d0f8b52ce1a7359bab6a4bc388e6c5532
Peter Mwaura <kimmwaus@gmail.com>
2024-07-01 14:53:05 -0700

    update
    Signed-off-by: Peter Mwaura <kimmwaus@gmail.com>

--- app/Telegram/CallBacks/HandleChatMessage.php
+++ app/Telegram/CallBacks/HandleChatMessage.php
@@ -3,9 +3,6 @@
 namespace App\Telegram\CallBacks;
 
 use App\Telegram\CallBacks\Home\Home;
-use DefStudio\Telegraph\Keyboard\Button;
-use DefStudio\Telegraph\Keyboard\Keyboard;
-use Illuminate\Support\Facades\Log;
 
 trait HandleChatMessage
 {
@@ -14,53 +11,54 @@ trait HandleChatMessage
 
     private function goToHome($text = null): void
     {
-        if (
-            !empty($this->getChat()->storage()->get('user_context')) &&
-            $this->getChat()->storage()->get('user_context') == 'phone_number_request_mode'
-        ) {
-            Log::info($this->validateNumber($text));
-
-            if (!$this->validateNumber($text)) {
-
-                //reset context
-                $this->getChat()->storage()->set('user_context', "");
-
-                //Advice user on corrections
-                $this?->getChat()
-                    ->message(
-                        "The number you entered was invalid use either \n
-                        07********
-                        +2547********
-                        2547********
-                        Formats
-                        ")
-                    ->keyboard(Keyboard::make()->row([
-                        Button::make('Try Again')
-                            ->action('invalid_phone_number_action')
-                            ->param('action', 'Try Again'),
-
-                        Button::make('Cancel')
-                            ->action('invalid_phone_number_action')
-                            ->param('action', 'Cancel'),
-                    ]))->send();
-            }else{
-//make a payment request
-                $this->getChat()->message("Please accept the payment request made to continue.");
-            }
-        }else{
-            //application not in subscription mode
-
-            $this->getChat()->message('Select a plan to proceed')
-                ->keyboard(Keyboard::make()->row([
-                    Button::make('Hourly Plan @Ksh 100')
-                        ->action('select_plan')
-                        ->param('plan', 'hourly'),
-
-                    Button::make('Day Plan @Ksh 1500')
-                        ->action('select_plan')
-                        ->param('plan', 'daily'),
-                ]))->send();
-        }
+        $this->getChat()->message("active");
+//        if (
+//            !empty($this->getChat()->storage()->get('user_context')) &&
+//            $this->getChat()->storage()->get('user_context') == 'phone_number_request_mode'
+//        ) {
+//            Log::info($this->validateNumber($text));
+//
+//            if (!$this->validateNumber($text)) {
+//
+//                //reset context
+//                $this->getChat()->storage()->set('user_context', "");
+//
+//                //Advice user on corrections
+//                $this?->getChat()
+//                    ->message(
+//                        "The number you entered was invalid use either \n
+//                        07********
+//                        +2547********
+//                        2547********
+//                        Formats
+//                        ")
+//                    ->keyboard(Keyboard::make()->row([
+//                        Button::make('Try Again')
+//                            ->action('invalid_phone_number_action')
+//                            ->param('action', 'Try Again'),
+//
+//                        Button::make('Cancel')
+//                            ->action('invalid_phone_number_action')
+//                            ->param('action', 'Cancel'),
+//                    ]))->send();
+//            }else{
+////make a payment request
+//                $this->getChat()->message("Please accept the payment request made to continue.");
+//            }
+//        }else{
+//            //application not in subscription mode
+//
+//            $this->getChat()->message('Select a plan to proceed')
+//                ->keyboard(Keyboard::make()->row([
+//                    Button::make('Hourly Plan @Ksh 100')
+//                        ->action('select_plan')
+//                        ->param('plan', 'hourly'),
+//
+//                    Button::make('Day Plan @Ksh 1500')
+//                        ->action('select_plan')
+//                        ->param('plan', 'daily'),
+//                ]))->send();
+//        }
 
 
     }
