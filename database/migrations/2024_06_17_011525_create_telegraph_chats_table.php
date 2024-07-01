e0da7f02a2b466142811329a305b32a8c016d052
Peter Mwaura <kimmwaus@gmail.com>
2024-06-30 22:52:45 -0700

    update x
    Signed-off-by: Peter Mwaura <kimmwaus@gmail.com>

--- database/migrations/2024_06_17_011525_create_telegraph_chats_table.php
+++ database/migrations/2024_06_17_011525_create_telegraph_chats_table.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
             $table->string('chat_id');
             $table->string('name')->nullable();
             $table->foreignId('telegraph_bot_id')->constrained('telegraph_bots')->cascadeOnDelete();
-            $table->text('user_data');
+            $table->text('user_data')->nullable();
             $table->timestamps();
             $table->unique(['chat_id', 'telegraph_bot_id']);
         });
