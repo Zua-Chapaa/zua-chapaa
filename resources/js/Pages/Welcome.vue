d109434f2b249983ab985969d198c8e55bf13141
Peter Mwaura <kimmwaus@gmail.com>
2024-07-02 13:01:35 -0700

    update
    Signed-off-by: Peter Mwaura <kimmwaus@gmail.com>

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,10 +6,12 @@ import {Head, Link} from '@inertiajs/vue3';
     <Head title="Welcome" />
     <div class="w-full h-[100vh] flex justify-center items-center flex-col">
         <img class="max-w-[300px] mb-[20px]" src="/storage/system/logo.png" alt="LOGO">
-        <h1 class="text-[28px] text-white px-[20px] text-center mb-4">Welcome to Shikisha Kitura</h1>
+        <h1 class="text-[28px] text-white px-[20px] text-center mb-4">Welcome to Shikisha Kakitu</h1>
         <Link class="btn btn-primary mb-4" :href="route('dashboard')">Account</Link>
         <Link class="btn btn-primary" :href="route('register')">Register</Link>
     </div>
+
+
 </template>
 
 
