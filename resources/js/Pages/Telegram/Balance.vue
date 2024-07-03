8b352f91c5c3c471d9eaecb2319d3cfb34dfcd46
Peter Mwaura <kimmwaus@gmail.com>
2024-07-03 04:16:09 -0700

    update
    Signed-off-by: Peter Mwaura <kimmwaus@gmail.com>

--- resources/js/Pages/Telegram/Balance.vue
+++ resources/js/Pages/Telegram/Balance.vue
@@ -1,17 +1,21 @@
 <script setup>
-import {Head, Link} from '@inertiajs/vue3';
+import {Head} from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Welcome"/>
-    <div class="w-full h-[100vh] flex justify-center items-center flex-col">
-        <img class="max-w-[300px] mb-[20px]" src="/storage/system/logo.png" alt="LOGO">
-        <h1 class="text-[28px] text-white px-[20px] text-center mb-4">Welcome to Shikisha Kakitu</h1>
-        <Link class="btn btn-primary mb-4" :href="route('dashboard')">Account</Link>
-        <Link class="btn btn-primary" :href="route('register')">Register</Link>
+    <div class="text-white flex">
+        <div class="logo w-20">
+            <img src="/storage/system/logo.png" alt="Logo">
+        </div>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                 class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                <path
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+            </svg>
+        </div>
     </div>
-
-
 </template>
 
 
